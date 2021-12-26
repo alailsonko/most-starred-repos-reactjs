bec5f445328b9e4b400bf36e668e00ba11dab4c3
alailson <aalailson3@gmail.com>
2021-12-26 00:28:04 -0300

    create fetch api

---
+++ src/services/api.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-param-reassign */
+type FetchOptions = {
+  params?: {
+    [key: string]: string | number | boolean;
+  };
+  url: string;
+  method?: string;
+  body?: any;
+};
+
+export default async function fetchAPI({
+  body,
+  params = {},
+  url = '',
+  method = 'GET'
+}: FetchOptions) {
+  const headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  };
+  if (Object.keys(params).length > 0) {
+    url = `${url}?${Object.keys(params)
+      .map((key) => `${key}=${params[key]}`)
+      .join('&')}`;
+  }
+
+  body = JSON.stringify(body);
+
+  return fetch(url, {
+    method,
+    headers,
+    body
+  })
+    .then((response) => response.json())
+    .catch((error) => error);
+}
