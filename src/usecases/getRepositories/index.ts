0b5dea99fbea5ad8d05c25c757ef5976a8fd68ec
alailson <aalailson3@gmail.com>
2021-12-27 02:50:18 -0300

    implements pagination

--- src/usecases/getRepositories/index.ts
+++ src/usecases/getRepositories/index.ts
@@ -1,15 +1,24 @@
-import { selector } from 'recoil';
+import { atom, selector } from 'recoil';
 import fetchAPI from '../../services/api';
 import endpoints from '../../endpoints';
 import repositoryMapper from '../../mappers/repositoryMapper';
 
+export const pageAtom = atom({
+  key: 'pageAtom',
+  default: 1
+});
+
 export const getRepositories = selector({
   key: 'GetRepositories',
-  get: async () => {
+  get: async ({ get }) => {
     return fetchAPI({
       url: endpoints.searchRepositories,
       params: {
-        sort: '=stars'
+        created: `:>2021-12-12`,
+        sort: '=stars',
+        order: '=desc',
+        per_page: '=5',
+        page: `=${get(pageAtom)}`
       }
     }).then(({ items }: { items: Array<any> }) => items.map((item) => repositoryMapper(item)));
   }
