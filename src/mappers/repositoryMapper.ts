ed2dbedecad113b304229bafc5877f1372a21f0c
alailson <aalailson3@gmail.com>
2021-12-26 23:36:45 -0300

    create repository mapper

---
+++ src/mappers/repositoryMapper.ts
@@ -0,0 +1,11 @@
+import { IRepository } from '../domain/models/repository';
+
+export default function repositoryMapper(item: any): IRepository {
+  return {
+    id: item.id as number,
+    description: item.description as string,
+    link: item.html_url as string,
+    name: item.name as string,
+    stars: item.stargazers_count as number
+  };
+}
