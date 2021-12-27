d757783a0617b747abd0476e5220b4955673ebe9
alailson <aalailson3@gmail.com>
2021-12-26 23:37:17 -0300

    test query getRepository

---
+++ src/usecases/getRepositories/__tests__/getRepositories.test.ts
@@ -0,0 +1,8 @@
+/* eslint-disable camelcase */
+import { snapshot_UNSTABLE } from 'recoil';
+import { getRepositories } from '../index';
+
+test('Test getRepositories', async () => {
+  const initialSnapshot = snapshot_UNSTABLE();
+  expect(await initialSnapshot.getPromise(getRepositories)).toHaveLength(30);
+});
