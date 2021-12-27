090882e36bc9f9b67a7d72b817fdfbefec9d3912
alailson <aalailson3@gmail.com>
2021-12-26 23:43:57 -0300

    add suspense on root

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
+
+import { RecoilRoot } from 'recoil';
 import RoutesApp from './routes';
 
 function App() {
   return (
-    <ChakraProvider>
-      <RoutesApp />
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+          <RoutesApp />
+        </Suspense>
+      </ChakraProvider>
+    </RecoilRoot>
   );
 }
 
